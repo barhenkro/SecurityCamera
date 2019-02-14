1412d2ad875f0a77154d0916644c437fd71c4622
bar-hen <bar.hen.kro@gmail.com>
2019-02-14 11:49:15 +0200

    Createrd Face class

---
+++ face.py
@@ -0,0 +1,23 @@
+import face_recognition
+
+
+class Face(object):
+    def __init__(self, id):
+        self._id = id
+        self._name = ""
+        self._face_encodings = []
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    def compare_face(self, unknown_face_encoding):
+        return True in face_recognition.compare_faces(self._face_encodings, unknown_face_encoding)
