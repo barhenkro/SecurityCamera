14f2fdbc18922b931f9f0b76744a0085f1b673c2
bar-hen <bar.hen.kro@gmail.com>
2019-03-11 21:23:08 +0200

    changed log_enrance function parameters

--- log_database.py
+++ log_database.py
@@ -3,7 +3,7 @@ import pickle
 from log import Log
 
 
-class FaceLogger(object):
+class LogDatabase(object):
     def __init__(self, file_name):
         self._logs = []
         self._file_name = file_name
@@ -16,9 +16,8 @@ class FaceLogger(object):
             with open(self._file_name, 'wb') as file_handler:
                 pickle.dump(self._logs, file_handler)
 
-    def log_entrance(self, face, frame, face_locations):
-        face_capture  = ''
-        self._logs.append(Log(face_capture))
+    def log_entrance(self, face_id, face_capture):
+        self._logs.append(Log(face_id, face_capture))
 
         with open(self._file_name, 'wb') as file_handler:
             pickle.dump(self._logs, file_handler)
