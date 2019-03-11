e0f68497519eac8ef1ee9384e6f5e2cfa83949cc
bar-hen <bar.hen.kro@gmail.com>
2019-03-11 21:19:45 +0200

    added face id in constructor

--- log.py
+++ log.py
@@ -2,7 +2,7 @@ import time
 
 
 class Log(object):
-    def __init__(self,face_capture):
+    def __init__(self, face_id, face_capture):
         self.__time = time.localtime()
-        self.__face_id = -1
+        self.__face_id = face_id
         self._face_capture = face_capture
