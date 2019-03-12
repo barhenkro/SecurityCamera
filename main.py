b2f743f25a8a1e8f1b2885f7448e4ec166f2d04b
bar-hen <bar.hen.kro@gmail.com>
2019-03-12 21:25:15 +0200

    dealed with a case when there are not faces in the database and a new face was detected

--- main.py
+++ main.py
@@ -6,23 +6,34 @@ import face_recognition
 
 
 def main():
-    video_capture = cv2.VideoCapture(0)
+    # video_capture = cv2.VideoCapture(0)
     face_database = FaceDatabase("faces.txt")
     log_database = LogDatabase('logs.txt')
+    frame = cv2.imread("obama2.jpg")
 
     while True:
-        _, frame = video_capture.read()
+        # _, frame = video_capture.read()
         face_locations = face_recognition.face_locations(frame)
-        face_encodings = face_recognition.face_encoding(frame, known_face_locations=face_locations)
+        face_encodings = face_recognition.face_encodings(frame, known_face_locations=face_locations)
         for face_encoding in face_encodings:
-            for face in face_database.faces:
-                if face.compare_face(face_encoding):
-                    log_database.log_entrance(face.id, frame)
 
-                else:
-                    new_face = Face(face_encoding, len(face_database.faces))
-                    face_database.add_face(new_face)
-                    log_database.log_entrance(new_face.id, frame)
+            # unknown face
+            if len(face_database.faces) == 0:
+                new_face = Face(face_encoding, len(face_database.faces))
+                face_database.add_face(new_face)
+                log_database.log_entrance(new_face.id, frame)
+
+            else:
+                for face in face_database.faces:
+                    # known face
+                    if face.compare_face(face_encoding):
+                        log_database.log_entrance(face.id, frame)
+
+                    # unknown face
+                    else:
+                        new_face = Face(face_encoding, len(face_database.faces))
+                        face_database.add_face(new_face)
+                        log_database.log_entrance(new_face.id, frame)
 
 
 if __name__ == '__main__':
