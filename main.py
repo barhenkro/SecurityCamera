ca77135e19ceb30d43f7c5924c50b7f216ed2181
bar-hen <bar.hen.kro@gmail.com>
2019-03-17 13:06:17 +0200

    fixed logic for face recognition

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ def main():
     face_database = FaceDatabase("faces.txt")
     log_database = LogDatabase('logs.txt')
     frame = cv2.imread("obama.jpg")
-    streamer = Streamer()
-    Streamer.run()
+    # streamer = Streamer()
+    # Streamer.run()
 
     while True:
         # _, frame = video_capture.read()
         face_locations = face_recognition.face_locations(frame)
-        face_encodings = face_recognition.face_encodings(frame, known_face_locations=face_locations)
-        for face_encoding in face_encodings:
+        # face_encodings = face_recognition.face_encodings(frame, known_face_locations=face_locations)
 
+        for face_location in face_locations:
+            face_encoding = face_recognition.face_encodings(frame, known_face_locations=[face_location])[0]
             # unknown face
             if len(face_database.faces) == 0:
                 new_face = Face(face_encoding, len(face_database.faces))
@@ -31,18 +32,21 @@ def main():
                     # known face
                     if face.compare_face(face_encoding):
                         log_database.log_entrance(face.id, frame)
+                        break
 
-                    # unknown face
-                    else:
-                        new_face = Face(face_encoding, len(face_database.faces))
-                        face_database.add_face(new_face)
-                        log_database.log_entrance(new_face.id, frame)
+                # unknown face
+                else:
+                    new_face = Face(face_encoding, len(face_database.faces))
+                    face_database.add_face(new_face)
+                    log_database.log_entrance(new_face.id, frame)
 
         # mark the faces
+        """
         for face_location in face_locations:
             (top, right, bottom, left) = face_location
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255))
         streamer.update(frame)
+        """
 
 
 if __name__ == '__main__':
