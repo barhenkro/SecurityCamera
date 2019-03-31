d0945528a22a6234bde3a5d0619d435db8b4cd32
bar-hen <bar.hen.kro@gmail.com>
2019-03-31 22:51:45 +0300

    added page to show face info, includes name changing

--- web_interface.py
+++ web_interface.py
@@ -1,6 +1,7 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request
 import cv2
 from threading import Thread
+from databases import *
 
 _app = Flask('securitycamera')
 camera_frame = 0
@@ -23,3 +24,11 @@ def stream():
 def get_img_bytes(img):
     _, img = cv2.imencode('.jpg', img)
     return img.tobytes()
+
+
+@_app.route('/face/<int:face_id>', methods=['POST', 'GET'])
+def show_face(face_id):
+    face = face_database[face_id]
+    if request.method == 'POST':
+        face.name = request.form['name']
+    return render_template('face.html', face=face)
