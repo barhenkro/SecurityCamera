5aa2c0de417984199d3b3b73329a21dee16a46b4
bar-hen <bar.hen.kro@gmail.com>
2019-04-03 22:24:44 +0300

    added general template for web page with list of links

--- web_interface.py
+++ web_interface.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template, request
+from flask import Flask, Response, render_template, request, url_for
 import cv2
 from threading import Thread
 from databases import *
@@ -26,7 +26,7 @@ def get_img_bytes(img):
     return img.tobytes()
 
 
-@_app.route('/face/<int:face_id>', methods=['POST', 'GET'])
+@_app.route('/faces/<int:face_id>', methods=['POST', 'GET'])
 def show_face(face_id):
     if request.method == 'POST':
         face_database.change_name(face_id, request.form['name'])
@@ -35,4 +35,11 @@ def show_face(face_id):
 
 @_app.route('/faces')
 def list_faces():
-    return render_template('faces.html', faces=face_database.faces)
+    initial_string = url_for('list_faces')+'/'
+    items = create_list_items(face_database.faces, lambda face: face.name, initial_string)
+    return render_template('list_page.html', title="Faces", list_items=items)
+
+
+def create_list_items(objects_list, naming_function, initial_string):
+    for index in range(len(objects_list)):
+        yield (naming_function(objects_list[index]), initial_string + str(index))
