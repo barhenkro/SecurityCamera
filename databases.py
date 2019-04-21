99c1ba0d38b5b28dbfeff6f02d6d808c50ed4c1a
bar-hen <bar.hen.kro@gmail.com>
2019-04-21 23:12:28 +0300

    added image database to the databases

--- databases.py
+++ databases.py
@@ -1,5 +1,7 @@
 from face_database import FaceDatabase
 from log_database import LogDatabase
+from image_database import ImageDatabase
 
 face_database = FaceDatabase('faces.txt')
 log_database = LogDatabase('logs.txt')
+image_database = ImageDatabase('image_counter', 'images')
