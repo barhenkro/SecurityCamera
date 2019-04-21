ffea9896d1ea8244149def68125cea9b44f10d64
bar-hen <bar.hen.kro@gmail.com>
2019-04-21 14:04:32 +0300

    added image database for saving images

---
+++ image_database.py
@@ -0,0 +1,32 @@
+import json
+import os
+import cv2
+
+
+class ImageDatabase(object):
+    def __init__(self, file_name, folder_name):
+        self._counter = 0
+        self._file_name = file_name
+        self._folder_name = folder_name
+
+        if os.path.exists(self._file_name):
+            with open(self._file_name, 'rb') as file_handler:
+                self._counter = json.load(file_handler)
+
+        else:
+            self._write_data()
+
+        if not os.path.isdir(self._folder_name):
+            os.mkdir(self._folder_name)
+
+    def _write_data(self):
+        with open(self._file_name, 'wb') as file_handler:
+            json.dump(self._counter, file_handler)
+
+    def save_image(self, image):
+        image_path = "{folder}/{name}.jpg".format(folder=self._folder_name, name=self._counter), image
+        cv2.imwrite(image_path, image)
+
+        self._counter += 1
+        self._write_data()
+        return image_path
