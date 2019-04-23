268d26609bcae5e841255c73cc37801eaab96fcd
bar-hen <bar.hen.kro@gmail.com>
2019-04-23 23:49:32 +0300

    added face croping

--- face_detection.py
+++ face_detection.py
@@ -12,11 +12,12 @@ def detect_faces(frame):
     face_locations = face_recognition.face_locations(frame)
 
     for face_location in face_locations:
+        face_capture = crop_face(frame, face_location)
         face_encoding = face_recognition.face_encodings(frame, known_face_locations=[face_location])[0]
 
         # unknown face
         if len(face_database) == 0:
-            register_new_face(face_encoding, image_database.save_image(frame))
+            register_new_face(face_encoding, image_database.save_image(face_capture))
 
         else:
             for face_id in range(len(face_database)):
@@ -27,14 +28,14 @@ def detect_faces(frame):
                     recognized_face = True
 
                     if registered_face.time_since_last_seen >= 5:
-                        log_database.log_entrance(face_id, image_database.save_image(frame))
+                        log_database.log_entrance(face_id, image_database.save_image(face_capture))
 
                     if registered_face.time_since_last_seen >= 1:
                         face_database.update_last_seen(face_id)
 
             # unknown face
             if not recognized_face:
-                register_new_face(face_encoding, image_database.save_image(frame))
+                register_new_face(face_encoding, image_database.save_image(face_capture))
 
     return frame
 
@@ -43,3 +44,8 @@ def register_new_face(face_encoding, capture_path):
     face_id = len(face_database)
     face_database.add_face(face_encoding, capture_path)
     log_database.log_entrance(face_id, capture_path)
+
+
+def crop_face(frame, face_location):
+    top, right, bottom, left = face_location
+    return frame[top:bottom+1, left:right+1]
