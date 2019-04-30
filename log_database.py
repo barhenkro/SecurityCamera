e926a900ff7a73cf5d393e64435664bd8d0d4b69
bar-hen <bar.hen.kro@gmail.com>
2019-05-01 01:14:47 +0300

    added property for unseen faces and function to update unseen data

--- log_database.py
+++ log_database.py
@@ -13,18 +13,27 @@ class LogDatabase(object):
                 self._logs = pickle.load(file_handler)
 
         else:
-            with open(self._file_name, 'wb') as file_handler:
-                pickle.dump(self._logs, file_handler)
+            self._write_data()
 
     def log_entrance(self, face_id, image_path):
         self._logs.append(Log(face_id, image_path))
+        self._write_data()
 
+    def update_is_seen(self, log_id):
+        self._logs[log_id].is_seen = True
+        self._write_data()
+
+    def _write_data(self):
         with open(self._file_name, 'wb') as file_handler:
             pickle.dump(self._logs, file_handler)
 
     @property
     def logs(self):
         return self._logs
+
+    @property
+    def unseen_faces(self):
+        return {index: self._logs[index] for index in range(len(self._logs)) if not self._logs[index].is_seen}
 
     def __getitem__(self, item):
         return self._logs[item]
