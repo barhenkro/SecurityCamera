bb980aa2fa97a9e37bdbcd2c5a057f91ae983720
bar-hen <bar.hen.kro@gmail.com>
2019-05-03 19:47:42 +0300

    added facroty for creating FaceDetector objects

---
+++ face_detector_factory.py
@@ -0,0 +1,22 @@
+from notifier import Notifier
+from face_detector import FaceDetector
+
+
+def create_face_detector(settings):
+    notifiers_settings = []
+    notifiers = []
+
+    if "notifiers" in settings:
+        notifiers_settings = settings['notifiers']
+
+    # build notifiers
+    for notifier_name, notifier_attributes in notifiers_settings.iteritems():
+        notifiers_registry = _get_notifiers_registry()
+        if notifier_name in notifiers_registry:
+            notifiers.append(notifiers_registry[notifier_name](**notifier_attributes))
+
+    return FaceDetector(notifiers)
+
+
+def _get_notifiers_registry():
+    return {notifier_class.__name__: notifier_class for notifier_class in Notifier.__subclasses__()}
