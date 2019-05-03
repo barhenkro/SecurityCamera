6f3eb387cfef09b23dbe4c108c97fd0799f8a002
bar-hen <bar.hen.kro@gmail.com>
2019-05-03 19:49:54 +0300

    added use of the face detector factory

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import cv2
-import face_detection
+from face_detector_factory import create_face_detector
 import web_interface
+import json
 
 
 def main():
     frame = cv2.imread("obama.jpg")
     web_interface.run()
 
+    with open('settings.json', 'rb') as file_handler:
+        settings = json.load(file_handler)
+
+    face_detector = create_face_detector(settings)
+
     while True:
-        web_interface.camera_frame = face_detection.detect_faces(frame)
+        web_interface.camera_frame = face_detector.detect_faces(frame)
 
 
 if __name__ == '__main__':
