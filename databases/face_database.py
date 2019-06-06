15e6128a60c1df00783f4869b534e73ba8f87997
bar-hen <bar.hen.kro@gmail.com>
2019-06-06 23:34:48 +0300

    added use of read_data before showing it

--- databases/face_database.py
+++ databases/face_database.py
@@ -9,10 +9,12 @@ class FaceDatabase(Database):
 
     @property
     def faces(self):
+        self._read_data()
         return copy(self._data)
 
     @property
     def unnamed_faces(self):
+        self._read_data()
         return {index: self._data[index] for index in range(self.__len__()) if self._data[index].name is None}
 
     def add_face(self, face_encoding, face_image_path, **kwargs):
@@ -20,25 +22,31 @@ class FaceDatabase(Database):
         if 'name' in kwargs:
             name = kwargs['name']
 
+        self._read_data()
         self._data.append(Face(face_encoding, face_image_path, name))
         self._write_data()
 
     def change_name(self, face_id, name):
+        self._read_data()
         self._data[face_id].name = name
         self._write_data()
 
     def update_last_seen(self, face_id):
+        self._read_data()
         self._data[face_id].update_last_seen()
         self._write_data()
 
     def compare_all_faces(self, face_encoding):
+        self._read_data()
         for face in self._data:
             if face.compare_face(face_encoding):
                 return True
         return False
 
     def __len__(self):
+        self._read_data()
         return len(self._data)
 
     def __getitem__(self, item):
+        self._read_data()
         return self._data[item]
--- databases/log_database.py
+++ databases/log_database.py
@@ -7,11 +7,13 @@ class LogDatabase(Database):
         super(LogDatabase, self).__init__(file_name)
 
     def log_entrance(self, face_id, image_path):
+        self._read_data()
         self._data.append(Log(face_id, image_path))
         self._write_data()
         return len(self._data) - 1  # the id of the log
 
     def update_is_seen(self, log_id):
+        self._read_data()
         self._data[log_id].is_seen = True
         self._write_data()
 
