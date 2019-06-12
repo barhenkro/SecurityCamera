0be92a51090077b90fc8fd21f850027529865ac0
bar-hen <bar.hen.kro@gmail.com>
2019-06-13 01:40:26 +0300

    created database for usernames and passwords

--- databases/__init__.py
+++ databases/__init__.py
@@ -1,7 +1,9 @@
 from face_database import FaceDatabase
 from image_database import ImageDatabase
 from log_database import LogDatabase
+from users_database import UsersDatabase
 
 face_database_instance = FaceDatabase('faces.txt')
 log_database_instance = LogDatabase('logs.txt')
 image_database_instance = ImageDatabase('image_counter')
+users_database_instance = UsersDatabase('users.json')
