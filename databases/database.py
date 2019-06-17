cf5b175927115d1a000a850569895a92a7e2d0ca
bar-hen <bar.hen.kro@gmail.com>
2019-06-18 00:00:10 +0300

    added option to multiple objects at once

--- databases/database.py
+++ databases/database.py
@@ -48,7 +48,8 @@ class Database(object):
         elif type(item) is list:
             return [self._data[index] for index in item]
 
-    def __delitem__(self, key):
+    def __delitem__(self, keys):
         self._read_data()
-        self._data[key] = None
+        for key in keys:
+            self._data[key] = None
         self._write_data()
